adb5e6081fbd65a8ba93d021c4dcd63bb75283a6
lazarok09 <lazarok09@gmail.com>
2023-12-31 13:19:23 -0300

    feat(auth): add cors to origin login

--- src/router/auth/index.ts
+++ src/router/auth/index.ts
@@ -1,12 +1,20 @@
 import express, { Response } from 'express';
 
 import AuthController from '../../controllers/auth';
+import cors from 'cors';
+
+const corsOptions = {
+  origin: process.env.PUBLIC_WEB_URL,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 export const AuthRouter = () => {
   const router = express.Router();
   // Auth
   router.use('/', express.json());
   router.use('/', express.urlencoded({ extended: true }));
+
+  router.use('/signin', cors(corsOptions));
   router.post('/signin', AuthController.signIn);
   router.post('/signout', AuthController.signOff);
 
