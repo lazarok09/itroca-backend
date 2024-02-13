889d60ade126746044f7c5af14646305bc8c4bd9
lazarok09 <lazarok09@gmail.com>
2024-02-13 15:31:12 -0300

    feat(user): add user resource with nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
