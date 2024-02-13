68b9cf4cbbcefa839f82585a918b5e8353edc220
lazarok09 <lazarok09@gmail.com>
2024-02-13 13:54:54 -0300

    BREAKING_CHANGE: start nestjs

--- src/types/request.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request } from 'express';
-
-interface CustomUserRequest extends Request {
-  user: VerifyJWTResultDecoded;
-}
--- src/utils/generic.ts
+++
@@ -1,3 +0,0 @@
-export const generateRandomNumber = (min = 1, max = 100) => {
-  return Math.floor(Math.random() * (max - min + 1) * min);
-};
