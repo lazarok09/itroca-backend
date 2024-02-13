d2f44c553f6e0f6d29ecaf54eafde48556444dbe
lazarok09 <lazarok09@gmail.com>
2024-02-13 13:57:31 -0300

    feat(prisma): add docker compose and new schema

--- prisma/migrations/20231124172855_add/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Product" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "image" TEXT NOT NULL,
-
-    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20231126174654_add/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `hash` on table `User` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "hash" SET NOT NULL;
