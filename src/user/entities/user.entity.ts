f28ac5883535c69a52f0d478613fa97500619569
lazarok09 <lazarok09@gmail.com>
2024-02-13 15:38:50 -0300

    feat(user): add user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Product } from '../../products/entities/product.entity';
+
+export class User {
+  id: number;
+  email: string;
+  name: string;
+  age: number;
+  address: string;
+  hash: string;
+  createdAt: Date;
+  updatedAt: Date;
+  products: Product[];
+}
