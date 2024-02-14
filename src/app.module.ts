577a2380964adf38820ec0514f5d63b3fd9e87b7
lazarok09 <lazarok09@gmail.com>
2024-02-13 21:04:43 -0300

    feat(auth): init auth handler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ProductsModule, UserModule],
+  imports: [ProductsModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
